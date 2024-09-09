1cf4bf8765f8663fce58154c5b48cbf41177492e
augusto faggion <a.faggion.santos@gmail.com>
2024-09-09 23:45:47 +0200

    added navbar to pages.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 
-export default function Home() {
+export default function Nabar() {
   return (
-    <div>
-      <main>
-        <h1>Workingg...</h1>
-      </main>
-    </div>
+    <nav>
+      <ul className={styles.navbar}>
+        <li>
+          {/* add logo here */}
+          <a href="/">
+            <Image src="/logo.svg" alt="Logo" width={100} height={100} />
+          </a>
+        </li>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li><a href="/about">About me</a></li>
+        <li><a href="/skills">Skills</a></li>
+        <li><a href="/contact">Contact me</a></li>
+        <li><a href="english">EN</a></li>
+      </ul>
+    </nav>
   );
 }
