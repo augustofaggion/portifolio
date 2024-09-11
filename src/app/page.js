91b08aa6517c28c5e91b97b228aa7c506051278d
augusto faggion <a.faggion.santos@gmail.com>
2024-09-11 11:12:02 +0200

    organized navbar and positioned

--- src/app/page.js
+++ src/app/page.js
@@ -3,24 +3,33 @@ import styles from "./page.module.scss";
 
 export default function App() {
   return (
-    <nav>
-      <ul className={styles.navbar}>
-        <li>
+    <nav className="navbar">
+      <ul className="navbar">
+        <li className="logo">
           {/* add logo here */}
-          <a href="/">
-            <Image src="/logo.svg" alt="Logo" width={100} height={100} />
-          </a>
+          <a href="/">Augusto</a>
         </li>
-        <li>
-          <a href="/home">Home</a>
-        </li>
-        <li>
-          <a href="/projects">Projects</a>
-        </li>
-        <li><a href="/about">About me</a></li>
-        <li><a href="/skills">Skills</a></li>
-        <li><a href="/contact">Contact me</a></li>
-        <li><a href="english">EN</a></li>
+
+        <div className="navItems">
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/projects">Projects</a>
+          </li>
+          <li>
+            <a href="/about">About me</a>
+          </li>
+          <li>
+            <a href="/skills">Skills</a>
+          </li>
+          <li>
+            <a href="/contact">Contact me</a>
+          </li>
+          <li>
+            <a href="english">EN</a>
+          </li>
+        </div>
       </ul>
     </nav>
   );
