f58b95c77276d7844021117da9e232c870f38c2d
augusto faggion <a.faggion.santos@gmail.com>
2024-09-13 23:24:49 +0200

    added responsiveness to navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,35 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
-
-import {Menu, X} from 'lucide-react';
+"use client";
+import { useState } from "react";
+import { Menu, X } from 'lucide-react';
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Function to toggle the navbar
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="navbar">
-        <li className="logo">
-          {/* add logo here */}
-          <a href="/">Augusto</a>
-        </li>
+      <div className="navbar__container">
+        <a href="/" className="navbar__logo">Augusto</a>
 
-        <div className="navItems">
-
-            <Menu className="open" size="24" />
-            <X className="close" size="24" />
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/projects">Projects</a>
-          </li>
-          <li>
-            <a href="/about">About me</a>
-          </li>
-          <li>
-            <a href="/skills">Skills</a>
-          </li>
-          <li>
-            <a href="/contact">Contact me</a>
-          </li>
-          <li>
-            <a href="english">EN</a>
-          </li>
+        {/* Toggle menu button */}
+        <div className="navbar__menu-icon" onClick={toggleNavbar}>
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
         </div>
-      </ul>
+      </div>
+
+      {/* Nav items - hide/show based on state */}
+      <div className={`navbar__items ${isOpen ? 'navbar__items--open' : ''}`}>
+        <a href="/home" className="navbar__link">Home</a>
+        <a href="/projects" className="navbar__link">Projects</a>
+        <a href="/about" className="navbar__link">About me</a>
+        <a href="/skills" className="navbar__link">Skills</a>
+        <a href="/contact" className="navbar__link">Contact me</a>
+        <a href="/english" className="navbar__link">EN</a>
+      </div>
     </nav>
   );
 }
