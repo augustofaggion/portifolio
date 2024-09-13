57c6f6de13dd7c212708713fdedac1117a86cdfa
augusto faggion <a.faggion.santos@gmail.com>
2024-09-13 18:17:34 +0200

    icons from lucid add and working

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 
+import {Menu, X} from 'lucide-react';
+
 export default function App() {
   return (
     <nav className="navbar">
@@ -11,6 +13,9 @@ export default function App() {
         </li>
 
         <div className="navItems">
+
+            <Menu className="open" size="24" />
+            <X className="close" size="24" />
           <li>
             <a href="/home">Home</a>
           </li>
