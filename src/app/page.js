04fe6d7e0d704050ee297c3eeef4dd9934b9cf60
augusto faggion <a.faggion.santos@gmail.com>
2024-09-16 10:30:42 +0200

    refractored navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,11 @@
-"use client";
-import { useState } from "react";
-import { Menu, X } from 'lucide-react';
+import Home from './components/home';
+import Navbar from './components/navbar';
 
 export default function App() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Function to toggle the navbar
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <nav className="navbar">
-      <div className="navbar__container">
-        <a href="/" className="navbar__logo">Augusto</a>
-
-        {/* Toggle menu button */}
-        <div className="navbar__menu-icon" onClick={toggleNavbar}>
-          {isOpen ? <X size={24} /> : <Menu size={24} />}
-        </div>
-      </div>
-
-      {/* Nav items - hide/show based on state */}
-      <div className={`navbar__items ${isOpen ? 'navbar__items--open' : ''}`}>
-        <a href="/home" className="navbar__link">Home</a>
-        <a href="/projects" className="navbar__link">Projects</a>
-        <a href="/about" className="navbar__link">About me</a>
-        <a href="/skills" className="navbar__link">Skills</a>
-        <a href="/contact" className="navbar__link">Contact me</a>
-        <a href="/english" className="navbar__link">EN</a>
-      </div>
-    </nav>
+    <>
+    <Navbar />
+    <Home />
+    </>
   );
 }
