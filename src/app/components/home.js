4cddcd77451bcf130aec3c839a198638a049726a
augusto faggion <a.faggion.santos@gmail.com>
2024-09-16 14:35:17 +0200

    home section finished

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -4,11 +4,12 @@ import PhotoBild from "../images/home.svg";
 export default function Home() {
   return (
     <section className="home container">
-
       <div className="home__left">
-        <h1>Hello, I&apos;m Augusto Santos</h1>
-        <h2>Fullstack Developer</h2>
-        <h3>Empowering your digital sucess with innovative solutions</h3>
+        <h1 className="home__header--primary">
+          Hello, I&apos;m Augusto Santos
+        </h1>
+        <h2 className="home__header--secondary">Fullstack Developer</h2>
+        <h3 className="home__header--terciary">Empowering your digital sucess with innovative solutions</h3>
       </div>
 
       <div className="home__right">
