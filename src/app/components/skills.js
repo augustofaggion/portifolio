1017f6becb627d6be5219d50828bbc196e14b428
augusto faggion <a.faggion.santos@gmail.com>
2024-09-21 00:37:43 +0200

    added list of skills

--- src/app/components/skills.js
+++ src/app/components/skills.js
@@ -1,22 +1,69 @@
 import Image from "next/image";
 // import PhotoBild from "../images/home.svg";
-import { Menu} from 'lucide-react';
 
-// maybe write skills, tech stack, etc
+
+const skills = [{
+  id: 1,
+  laguage: "Javascript",
+  logo: "test",
+},
+{id: 2,
+  laguage: "Ruby",
+  logo: "test",
+},
+{id: 3,
+  laguage: "React",
+  logo: "test",
+},
+{id: 4,
+  laguage: "Ruby on Rails",
+  logo: "test",
+},
+{id: 5,
+  laguage: "HTML",
+  logo: "test",
+},
+{id: 6,
+  laguage: "CSS",
+  logo: "test",
+},
+{id: 7,
+  laguage: "SASS",
+  logo: "test"},
+{id: 8,
+  laguage: "Bootstrap",
+  logo: "test",
+},
+{id:9,
+  laguage: "SQL",
+  logo: "test",
+},
+{id: 10,
+  laguage: "PostgreSQL",
+  logo: "test",
+},
+// Check for SQL which is best to write
+{id: 11,
+  laguage: "Git",
+  logo: "test",
+}, {id: 12,
+  language: "Figma"
+}
+
+];
 
 export default function Skills() {
   return (
-    <section className="project container">
-      <h1>Skills</h1>
-      <div class="project__box">
-        <Image alt="testing"/>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam ex
-          perferendis praesentium, tempore aut earum pariatur repudiandae at.
-          Cupiditate ullam iure veniam et voluptatibus impedit non voluptatem
-          ratione ipsum explicabo!
-        </p>
-        <Menu size={24} />
+    <section className="skills container">
+      <h2 className="skills_name">Skills</h2>
+      <div class="skills__box">
+        {skills.map((skill)=> (
+          <div key={skill.id} className="skills_content">
+            {/* <Image src={skill.logo} alt={skill.language} width={300} height={300} /> */}
+            <p className="skill__text">{skill.laguage}</p>
+          </div>
+        ))}
+
       </div>
     </section>
   );
