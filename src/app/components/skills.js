74ccd06be48f24a29cdb7acb750471673f89aefe
augustofaggionliradossantos <a.faggion.santos@gmail.com>
2024-09-21 11:41:44 +0200

    skills logos add

--- src/app/components/skills.js
+++ src/app/components/skills.js
@@ -1,55 +1,30 @@
 import Image from "next/image";
-// import PhotoBild from "../images/home.svg";
+// Links for the logos
+import javascript from "../images/skills/javascript.svg";
+import ruby from "../images/skills/ruby.svg";
+import react from "../images/skills/react.svg";
+import rails from "../images/skills/rails.svg";
+import html from "../images/skills/html.svg";
+import css from "../images/skills/css.svg";
+import sass from "../images/skills/sass.svg";
+import bootstrap from "../images/skills/bootstrap.svg";
+import sql from "../images/skills/sql.svg";
+import git from "../images/skills/git.svg";
+import figma from "../images/skills/figma.svg";
 
-
-const skills = [{
-  id: 1,
-  laguage: "Javascript",
-  logo: "test",
-},
-{id: 2,
-  laguage: "Ruby",
-  logo: "test",
-},
-{id: 3,
-  laguage: "React",
-  logo: "test",
-},
-{id: 4,
-  laguage: "Ruby on Rails",
-  logo: "test",
-},
-{id: 5,
-  laguage: "HTML",
-  logo: "test",
-},
-{id: 6,
-  laguage: "CSS",
-  logo: "test",
-},
-{id: 7,
-  laguage: "SASS",
-  logo: "test"},
-{id: 8,
-  laguage: "Bootstrap",
-  logo: "test",
-},
-{id:9,
-  laguage: "SQL",
-  logo: "test",
-},
-{id: 10,
-  laguage: "PostgreSQL",
-  logo: "test",
-},
-// Check for SQL which is best to write
-{id: 11,
-  laguage: "Git",
-  logo: "test",
-}, {id: 12,
-  language: "Figma"
-}
-
+// Skills data
+const skills = [
+  { id: 1, laguage: "Javascript", logo: javascript },
+  { id: 2, laguage: "Ruby", logo: ruby },
+  { id: 3, laguage: "React", logo: react },
+  { id: 4, laguage: "Ruby on Rails", logo: rails },
+  { id: 5, laguage: "HTML", logo: html },
+  { id: 6, laguage: "CSS", logo: css },
+  { id: 7, laguage: "SASS", logo: sass },
+  { id: 8, laguage: "Bootstrap", logo: bootstrap },
+  { id: 9, laguage: "PostgreSQL", logo: sql },
+  { id: 10, laguage: "Git", logo: git },
+  { id: 11, language: "Figma", logo: figma },
 ];
 
 export default function Skills() {
@@ -57,13 +32,17 @@ export default function Skills() {
     <section className="skills container">
       <h2 className="skills_name">Skills</h2>
       <div class="skills__box">
-        {skills.map((skill)=> (
+        {skills.map((skill) => (
           <div key={skill.id} className="skills_content">
-            {/* <Image src={skill.logo} alt={skill.language} width={300} height={300} /> */}
+            <Image
+              src={skill.logo}
+              alt={skill.language}
+              width={300}
+              height={300}
+            />
             <p className="skill__text">{skill.laguage}</p>
           </div>
         ))}
-
       </div>
     </section>
   );
