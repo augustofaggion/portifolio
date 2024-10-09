46b0fce8374acb2c13522b77c9c563d244a5aaf9
augustofaggion <a.faggion.santos@gmail.com>
2024-10-09 10:42:03 +0200

    home section finish

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -9,24 +9,21 @@ export default function Home() {
   return (
     <>
       <section className="home container" id="home">
-        <div className="home__left">
+
           <h1 className="home__header--primary" id="typeit">
             Hello, {name}
           </h1>
           <h2 className="home__header--secondary">Fullstack Developer</h2>
-          <h3 className="home__header--terciary">
-            Empowering your digital sucess with innovative solutions
-          </h3>
+
           <div className="home__cta">
             <a href="#contact" className="home__btn">
               Contact me
             </a>
           </div>
-        </div>
+        
 
-        <div className="home__right">
-          <Image src={homePhoto} alt="Augusto Santos" className="home__img"/>
-        </div>
+
+
       </section>
     </>
   );
